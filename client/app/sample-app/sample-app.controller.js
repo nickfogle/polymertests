1cddc82f7e1d5bc72c95512535a2cfed983712b1
nickfogle <nickfogle@gmail.com>
2014-08-11 22:57:09 -0400

    adding routes for sample-app

--- client/app/sample-app/sample-app.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('polymertestApp')
-  .controller('SampleAppCtrl', function ($scope, $http, Auth, User) {
-
-    $http.get('/api/users').success(function(users) {
-      $scope.users = users;
-    });
-  });
